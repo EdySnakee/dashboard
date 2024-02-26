9ed424604ba7540281001cbb0b09d3d5d61520e6
Edy <contactoedysnake@gmail.com>
2024-02-26 11:17:23 -0500

    add

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,20 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+type Grade = 'A' | 'B' | 'F';
 @Component({
   selector: 'control-flow',
   standalone: true,
   imports: [
     CommonModule,
   ],
-  template: `<p>control-flow works!</p>`,
+  templateUrl: './control-flow.component.html',
   styles: `
-    :host {
-      display: block;
-    }
+
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class ControlFlowComponent {
+
+  public showContent = signal(false);
+  public grade = signal<Grade>('A');
+
+  public toggleContent() {
+    this.showContent.update(value => !value);
+  }
 
 }
